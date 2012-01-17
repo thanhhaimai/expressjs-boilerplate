b3533b7dab3491f00efbcb5b712fe5b3a5d25650
Thanh Hai Mai <thanhhaipmai@gmail.com>
2012-01-16 21:18:41 -0800

    - use new 404 error page

--- routes/index.js
+++ routes/index.js
@@ -10,11 +10,10 @@ module.exports = function(app) {
     var name = file.substr(0, file.indexOf('.'));
     require('./' + name)(app);
   });
-
   /** Redirect everything else back to default if logged in. */
   app.get('*', function(req, res) {
-    util.log('GET URL: ' + req.url);
-    req.flash('error', "Whoops! The url you just went to does not exist.");
-    res.redirect('/default');
+    res.render('errors/404', {
+      status: 404
+    });
   });
 }
