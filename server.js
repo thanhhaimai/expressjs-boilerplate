c09794191deb97a142bcf5afd02646d8100c1233
Thanh Hai Mai <thanhhaipmai@gmail.com>
2012-01-16 18:34:39 -0800

    - move const to config.js - organize configuration for different environments - various improvements

--- server.js
+++ server.js
@@ -1,60 +1,82 @@
 /** Setting up dependencies. */
+var fs = require('fs');
+var util = require('util');
 var express = require('express');
-var app = module.exports = express.createServer();
 var mongoose = require('mongoose');
 var mongoStore = require('connect-mongodb');
-var fs = require('fs');
-var nodemailer = require('nodemailer');
 var sanitizer = require('validator').sanitize;
 
-/** Config. */
-var config = JSON.parse(fs.readFileSync("private/config"));
-var port = process.env.PORT || config.port;
-
-/** Database. */
+/** .*/
+var config = require('./config');
+var app = module.exports = express.createServer();
 var db;
-app.set('db-uri', config.dburi);
 
-/** Default cookie lifetime is 1 day. */
-var COOKIE_LIFETIME = 1000 * 60 * 60 * 24;
-/** Default fav icon lifetime is 30 days. */
-var FAVICON_LIFETIME = 1000 * 60 * 60 * 24 * 30
+/** Database models. */
+require('./models').defineModels(mongoose, app, function() {
+  db = mongoose.connect(config.dburi);
+});
+
+/** .*/
+process.addListener('uncaughtException', function(err, stack) {
+  util.log('Caught exception: ' + err + '\n' + err.stack);
+  util.log('\u0007');
+});
 
 /** Flash message support. */
 app.helpers(require('./dh.js').helpers);
 app.dynamicHelpers(require('./dh.js').dynamicHelpers);
-
-/** Database models. */
-require('./models').defineModels(mongoose, function() {
-  app.User = User = mongoose.model('User');
-  app.LoginToken = LoginToken = mongoose.model('LoginToken');
-  db = mongoose.connect(app.set('db-uri'));
-});
-
-/** Set up server, session management. */
-app.use(express.favicon(__dirname + '/public/favicon.ico', {
-  maxAge: FAVICON_LIFETIME
-}));
-app.use(express.bodyParser());
-app.use(express.cookieParser());
-app.use(express.session({
-  secret: 'this is not a secret secret',
-  store: mongoStore(db)
-}));
-app.use(express.static(__dirname + '/public'));
-
-//app.use(loadUser);
-
-app.use(app.router);
-app.use(express.errorHandler({
-  showStack: true,
-  dumpExceptions: true
-}));
-
 /** Where to look for templates. */
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
+/** Set up server, session management. */
+app.use(express.favicon(__dirname + '/public/favicon.ico', {
+  maxAge: config.FAVICON_LIFETIME
+}));
 
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.session({
+  secret: config.secret,
+  store: mongoStore(db)
+}));
+
+app.use(express.logger({
+  format: ':req[x-real-ip] :date (:response-time ms): :method :url'
+}));
+
+app.use(express.static(__dirname + '/public', {
+  maxAge: config.COOKIE_LIFETIME
+}));
+
+app.use(app.router);
+
+/** Show all errors and keep search engines out using robots.txt .*/
+app.configure('development', function() {
+  app.use(express.errorHandler({
+    showStack: true,
+    dumpExceptions: true
+  }));
+  app.all('/robots.txt', function(req, res) {
+    res.send('User-agent: *\nDisallow: /', {
+      'Content-Type': 'text/plain'
+    });
+  });
+});
+/** Suppress errors, allow all search engines .*/
+app.configure('production', function() {
+  app.use(express.errorHandler({
+    dumpExceptions: true
+  }));
+  app.all('/robots.txt', function(req, res) {
+    res.send('User-agent: *', {
+      'Content-Type': 'text/plain'
+    });
+  });
+});
+
+/** Load all the routes. */
 require('./routes')(app);
 
-app.listen(port);
+/** Start listenning. */
+app.listen(config.port);
+util.log(util.format('ENV: %s, listening on http://localhost:%s', config.env, config.port));
